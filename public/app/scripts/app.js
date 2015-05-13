9905ab8b4f22670bd0dacab4dd8b099f4cab08dd
Filip Lindvall <filip9719@gmail.com>
2015-05-14 00:01:17 +0200

    Created the angular frontend, this is based on some node modules and ruby compass

---
+++ public/app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name publicApp
+ * @description
+ * # publicApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('publicApp', [
+    'ngAnimate',
+    'ngResource',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
