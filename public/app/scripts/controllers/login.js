5e42ddb2f0dc148a9df6d9c4fafeebd9ce4037f2
Filip Lindvall <filip9719@gmail.com>
2015-05-14 00:34:11 +0200

    Added some new controllers that we probably need

---
+++ public/app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name publicApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the publicApp
+ */
+angular.module('riskApp')
+  .controller('LoginCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ public/app/scripts/controllers/stats.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name publicApp.controller:StatsCtrl
+ * @description
+ * # StatsCtrl
+ * Controller of the publicApp
+ */
+angular.module('riskApp')
+  .controller('StatsCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
