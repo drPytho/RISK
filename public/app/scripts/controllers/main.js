3cc10adbb729847ee827b5eef96cd46b34a61911
Filip Lindvall <filip9719@gmail.com>
2015-05-14 00:27:51 +0200

    Updated settings file

--- public/app/scripts/controllers/main.js
+++ public/app/scripts/controllers/main.js
@@ -7,7 +7,7 @@
  * # MainCtrl
  * Controller of the publicApp
  */
-angular.module('publicApp')
+angular.module('riskApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
