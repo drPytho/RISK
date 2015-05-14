4e5767e0b74081089c3906ca7f473192cbafcbdb
Filip Lindvall <filip9719@gmail.com>
2015-05-14 10:57:21 +0200

    Instaleld some new dependencies and updated app.js to use them

--- server/http/app.js
+++ server/http/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var passport = require('passport');
+var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -17,10 +19,16 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
+app.use(session{
+	secret:"Change me for production"
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//TODO cahnge public to own folder
+app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(passport.initialize());
+app.use(passport.sesstion());
 
 app.use('/api', api);
 app.use('/auth', auth);
