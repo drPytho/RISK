42b4fbf92cbd0e3f3c52e1d666cdc86033a2c091
Sawny <sawny@wallstrom.it>
2015-05-26 16:07:31 +0200

    Small fixes

--- web-server/public/scripts/controllers/main.js
+++ web-server/public/scripts/controllers/main.js
@@ -8,10 +8,6 @@
  * Controller of the publicApp
  */
 angular.module('riskApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope) {
+        
+    });
--- web-server/web-server.js
+++ web-server/web-server.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
 
+// All CSS, JS and images files
 app.use(express.static(__dirname +'/public'));
 
+// Load index.html which will start the Angular.js app
 app.get('/', function (req, res) {
     res.sendfile(__dirname +'/public/index.html');
 });
+
+// TODO: API for login, register, highscore etc
 
 var server = app.listen(3000, function () {
     var host = server.address().address;
